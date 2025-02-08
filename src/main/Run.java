760977d3df788af0d2bd4483a6284fd8ebab8633
Alfred <arista21dominguez@gmail.com>
2025-02-07 20:27:12 -0600

    the largest and smallest number of a string

---
+++ src/main/Run.java
@@ -0,0 +1,16 @@
+
+package main;
+
+import main.exercises.HighestAndLowest;
+
+/**
+ *
+ * @author Alfred
+ */
+public class Run
+{
+    public static void main(String[] args)
+    {
+        HighestAndLowest.run();
+    }
+}
